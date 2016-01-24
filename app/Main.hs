3c59df7b36dd00ac5de7d92de121632f7c7aa167
erik stevenson <eriknstevenson@gmail.com>
2016-01-24 18:47:09 -0500

    using sdl2

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Chip8
 
+import Control.Concurrent (threadDelay)
+import Foreign.C.Types
+import Linear
+import qualified SDL
+
+screenWidth, screenHeight :: CInt
+(screenWidth, screenHeight) = (640, 320)
+
 main :: IO ()
-main = putStrLn "Chip8 Interpreter"
+main = do
+  SDL.initialize [SDL.InitVideo]
+
+  window <- SDL.createWindow "Chip8" SDL.defaultWindow { SDL.windowInitialSize = V2 screenWidth screenHeight }
+  SDL.showWindow window
+
+  screenSurface <- SDL.getWindowSurface window
+  let white = V4 maxBound maxBound maxBound maxBound
+  SDL.surfaceFillRect screenSurface Nothing white
+  SDL.updateWindowSurface window
+
+  threadDelay 2000000
+
+  SDL.destroyWindow window
+  SDL.quit
+
+
